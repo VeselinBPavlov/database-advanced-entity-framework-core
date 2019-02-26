ba100883e9e8bf211010e8e564b5ef545a2c6458
VeselinBPavlov <veselin.b.pavlov@gmail.com>
2019-02-26 13:01:03 +0200

    Added MiniORM.

---
+++ 03. ORM Fundamentals - Lab and Exercise/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using Data;
+    using Data.Entities;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var connectionString = @"Server=(localdb)\MyInstance;Database=MiniORM;Integrated security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gancho",
+                LastName = "Ganchov",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Hello";
+
+            context.SaveChanges();
+        }
+    }
+}
