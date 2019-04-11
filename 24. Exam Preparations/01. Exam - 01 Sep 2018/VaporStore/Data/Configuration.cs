4e48fb2b93555c88b6d147b492e17e1e6c198565
VeselinBPavlov <veselin.b.pavlov@gmail.com>
2019-04-11 07:50:30 +0300

    Added certificate.

---
+++ 24. Exam Preparations/01. Exam - 01 Sep 2018/VaporStore/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace VaporStore.Data
+{
+	public static class Configuration
+	{
+		public static string ConnectionString =
+            @"Server=(localdb)\MyInstance;AttachDbFilename=D:\Courses\Data\VaporStore_Data.mdf;Database=VaporStore;Integrated Security=true;";
+	}
+}
